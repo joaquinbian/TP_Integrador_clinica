a4c2cda7ec045dd886fc35c7e2ba89d89038ea18
Federico Fogliatto <fede.fogliatto@gmail.com>
2024-10-10 19:34:53 -0300

    menues

--- Paciente.h
+++ Paciente.h
@@ -10,6 +10,7 @@ public:
     Paciente();
     Paciente(std::string nombre, std::string apellido, std::string telefono, std::string direccion, std::string ciudad, std::string email, Fecha fechaNacimiento, int edad, std::string obraSocial, std::string dni);
     void setFechaNacimiento(Fecha fechaNacimiento);
+    //void setFechaTurno(Fecha fechaTurno);
     void setEdad(int edad);
     void setObraSocial(std::string obraSocial);
     void setDni(std::string Dni);
@@ -23,6 +24,7 @@ public:
 
 private:
     Fecha _fechaNacimiento;
+    Fecha _fechaTurno;
     int _edad;
     char _dni[20];
     char _obraSocial[20];
--- main.cpp
+++ main.cpp
@@ -8,45 +8,134 @@ using namespace std;
 
 int main()
 {
-    int opcion;
+    int opcion, opcionPaciente, opcionProfesional;
+    char salir;
     Paciente p1;
     Profesional pr1;
 
     while(true)
     {
+        cout << "\033[4m";  // Comienza subrayado
         cout<<"SISTEMA DE GESTION DE TURNOS " <<endl<<endl;
-        cout<<"1 - SOLICITAR TURNO " <<endl;
-        cout<<"2 - MODIFICAR TURNO " <<endl;
-        cout<<"3 - ELIMINAR TURNO " <<endl;
-        cout<<"4 - LISTAR TURNOS " <<endl;
-        cout<<"5 - LISTAR PACIENTES " <<endl;
-        cout<<"6 - LISTAR PROFESIONALES " <<endl;
-        cout<<"7 - CARGAR PROFESIONAL " <<endl<<endl;
+        cout << "\033[0m";  // Detiene subrayado
+
+        cout<<"1 - PACIENTES" <<endl;
+        cout<<"2 - PROFESIONALES " <<endl;
+        cout<<"3 - LISTADOS " <<endl;
+        cout<<"0 - SALIR " <<endl<<endl;
+        /*cout<<"6 - LISTAR PACIENTES " <<endl;
+        cout<<"7 - CARGAR PROFESIONAL " <<endl;
+        cout<<"8 - LISTAR PROFESIONALES " <<endl<<endl;*/
 
         cout<<"Ingrese una opcion : ";
         cin>>opcion;
 
         switch(opcion)
         {
-        case 1 :
-            system("cls");
-            guardarPaciente();
-            system("pause");
-            mostrarTodosPacientes();
-            cout<<"Paciente cargado exitosamente " <<endl;
-            system("pause");
-            system("cls");
-            break;
-        case 7 :
-            system("cls");
-            pr1 = cargarProfesional();
-            cout<<"Profesional cargado exitosamente " <<endl;
-            system("pause");
-            mostrarProfesional(pr1);
-            system("pause");
-            system("cls");
-            break;
+        case 1:
+            while (true)
+            {
+                system("cls");
+                cout << "\033[4m";  // Comienza subrayado
+                cout<<"PACIENTES " <<endl<<endl;
+                cout << "\033[0m";  // Detiene subrayado
 
+                cout << "1 - CARGAR PACIENTE" << endl;
+                cout << "2 - ASIGNAR TURNO" << endl;
+                cout << "3 - MODIFICAR TURNO" << endl;
+                cout << "4 - ELIMINAR TURNO" << endl;
+                cout << "0 - VOLVER AL MENU PRINCIPAL" << endl << endl;
+
+                cout << "Ingrese una opcion: ";
+                cin >> opcionPaciente;
+
+                switch (opcionPaciente)
+                {
+                case 1:
+                    system("cls");
+                    cout << "\033[4m";  // Comienza subrayado
+                    cout << "DATOS DEL PACIENTE" << endl<<endl;
+                    cout << "\033[0m";  // Detiene subrayado
+                    cargarPaciente();
+                    break;
+                case 0:
+                    system("cls");
+                    break;
+                default:
+                    cout << "Opcion no valida." << endl;
+                }
+
+                if (opcionPaciente == 0)
+                {
+                    break;
+                }
+            }
+            break;
+        case 2:
+            while (true)
+            {
+                system("cls");
+                cout << "\033[4m";  // Comienza subrayado
+                cout<<"PROFESIONALES " <<endl<<endl;
+                cout << "\033[0m";  // Detiene subrayado
+
+                cout << "1 - CARGAR PROFESIONAL" << endl;
+                cout << "2 - MODIFICAR PROFESIONAL" << endl;
+                cout << "3 - ELIMINAR PROFESIONAL" << endl;
+                cout << "0 - VOLVER AL MENU PRINCIPAL" << endl << endl;
+
+                cout << "Ingrese una opcion: ";
+                cin >> opcionProfesional;
+
+                switch(opcionProfesional)
+                {
+                case 1:
+                    system("cls");
+                    cout << "\033[4m";  // Comienza subrayado
+                    cout<<"DATOS DEL PROFESIONAL " <<endl<<endl;
+                    cout << "\033[0m";  // Detiene subrayado
+                    cargarProfesional();
+                    break;
+                case 0:
+                    system("cls");
+                    break;
+                default:
+                    cout << "Opcion no valida." << endl;
+                }
+
+                if (opcionProfesional == 0)
+                {
+                    break;
+                }
+
+
+            }
+            break;
+        case 0:
+            do
+            {
+                system("cls");
+                cout<<"Desea salir ? S/N : " <<endl;
+                cin>>salir;
+                system("cls");
+
+
+                if(salir == 's' || salir == 'S')
+                {
+                    exit(0);
+
+                }
+                else if(salir == 'n' || salir == 'N')
+                {
+                    break;
+                }
+            }
+            while(salir != 's' || salir != 'S' || salir != 'n' || salir != 'N');
+
+            if (salir == 'n' || salir == 'N')
+            {
+                break;
+            }
 
 
 
@@ -65,3 +154,5 @@ int main()
 
     return 0;
 }
+
+
