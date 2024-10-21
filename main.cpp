7b853a98d7ded0727276e410dc3ab62131305668
Santiago Carletti <carlettisantiago@gmail.com>
2024-10-21 22:54:47 +0200

    Agregado de opción 'turnos' en main

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main()
 {
-    int opcion, opcionPaciente, opcionProfesional, opcionListado;
+    int opcion, opcionPaciente, opcionProfesional, opcionListado, opcionTurno;
     char salir;
     Paciente p1;
     Profesional pr1;
@@ -23,6 +23,7 @@ int main()
         cout<<"1 - PACIENTES" <<endl;
         cout<<"2 - PROFESIONALES " <<endl;
         cout<<"3 - LISTADOS " <<endl;
+        cout<<"4 - TURNOS " <<endl;
         cout<<"0 - SALIR " <<endl<<endl;
 
         cout<<"Ingrese una opcion : ";
@@ -153,6 +154,50 @@ int main()
                 }
             }
             break;
+                case 4:
+            while (true)
+            {
+                system("cls");
+                cout << "\033[4m";  // Comienza subrayado
+                cout<<"TURNOS " <<endl<<endl;
+                cout << "\033[0m";  // Detiene subrayado
+
+                cout << "1 - EDITAR OPCION 1" << endl;
+                cout << "2 - EDITAR OPCION 2" << endl;
+                cout << "0 - VOLVER AL MENU PRINCIPAL" << endl << endl;
+
+                cout << "Ingrese una opciÓn: ";
+                cin >> opcionTurno;
+
+                switch(opcionTurno)
+                {
+                case 1:
+                    system("cls");
+                    cout << "\033[4m";  // Comienza subrayado
+                    cout<<"SECCION OPCION 1" <<endl<<endl;
+                    cout << "\033[0m";  // Detiene subrayado
+                    system("pause");
+                    break;
+                case 2:
+                    system("cls");
+                    cout << "\033[4m";  // Comienza subrayado
+                    cout<<"SECCION OPCION 2" <<endl<<endl;
+                    cout << "\033[0m";  // Detiene subrayado
+                    system("pause");
+                    break;
+                case 0:
+                    system("cls");
+                    break;
+                default:
+                    cout << "Opción no válida." << endl;
+                }
+
+                if (opcionTurno == 0)
+                {
+                    break;
+                }
+            }
+                    break;
         case 0:
             do
             {
