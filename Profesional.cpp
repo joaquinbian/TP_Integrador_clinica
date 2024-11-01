93c1110edff1e8f9ae2e706b01a036eed20e7afb
joaquinbian <joacobian21@gmail.com>
2024-11-01 19:57:08 -0300

    implementacion del metodo mostrar de profesional

--- Profesional.cpp
+++ Profesional.cpp
@@ -50,3 +50,16 @@ const bool Profesional::getSoloParticular() const
 {
     return _soloParticular;
 }
+
+void Profesional::mostrar(){
+    cout<<"Nombre : " <<this->getNombre() <<endl;
+    cout<<"Apellido : " <<this->getApellido() <<endl;
+    cout<<"Domicilio : " <<this->getDireccion() <<endl;
+    cout<<"Ciudad : " <<this->getCiudad() <<endl;
+    cout<<"Telefono : " <<this->getTelefono() <<endl;
+    cout<<"Email : " <<this->getEmail() <<endl;
+    cout<<"Matricula : " <<this->getMatricula() <<endl;
+    cout<<"Especialidad : " <<this->getEspecialidad() <<endl;
+    cout<<"Valor de la consulta : $" <<this->getValorConsulta() <<endl;
+    cout<<"Atiende obras sociales y prepagas : " <<this->getSoloParticular() <<endl;
+}
